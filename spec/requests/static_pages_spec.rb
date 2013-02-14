6cce881479b1de97fa32fcfd7ed6fcef178ef790
cusanovich <darrencusanovich@gmail.com>
2013-02-14 05:14:46 -0600

    Work on static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,43 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Codecruiters'" do
+    it "should have the h1 'Codecruiters'" do
       visit '/static_pages/home'
-      page.should have_content('Codecruiters')
+      page.should have_selector('h1', :text => 'Codecruiters')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Codecruiters | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Codecruiters | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Codecruiters | About Us")
     end
   end
 end
