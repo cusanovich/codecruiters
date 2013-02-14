312d8358ff31d7c5dba484d51b55df1cbd53f349
cusanovich <darrencusanovich@gmail.com>
2013-02-14 00:04:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Codecruiters
+
+This is the application for [*Codecruiters*](http://codecruiters.com/)
+by [Darren Cusanovich](http://lnkd.in/qd_eiD)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codecruiters::Application.config.secret_token = '9d9a3e59a2e654438c65d9c7fe3ef878c7f75493764f2657c0906e2e247fd1f9a88a820e0d3f6ae3d63d94650ac0e0ce35fa7c348230bb220c15e0b0dba0c3db'
